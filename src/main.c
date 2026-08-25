5ff5727378b65eea0048a647fca41698d16c46c8
ERK191 <erick.andrei.dobre@gmail.com>
2026-08-25 16:10:20 +0300

    Update UEFI bootloader to version 0.0.2

--- src/main.c
+++ src/main.c
@@ -1,7 +1,72 @@
-// SMR-OS v0.0.1 - Initial UEFI Bootloader
+// ============================================================================
+// SMR-OS v0.0.2 - SISTEM GRAFIC ȘI SUPORT FIȘIERE (64-bit UEFI)
+// ============================================================================
+
+#pragma comment(linker, "/subsystem:efi_application")
+#pragma comment(linker, "/entry:EfiMain")
+
 unsigned long long typedef UINTN;
+unsigned int typedef UINT32;
 short typedef CHAR16;
 
+// Structură pentru Ghidul Universal de Identificare UEFI (GUID)
+typedef struct {
+    UINT32 Data1;
+    short  Data2;
+    short  Data3;
+    char   Data4[8];
+} EFI_GUID;
+
+#define EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID {0x41351778, 0x42e5, 0x4cd2, {0x8e, 0x3a, 0x9e, 0xcc, 0x54, 0x57, 0x73, 0xc1}}
+#define EFI_SIMPLE_FILE_SYSTEM_PROTOCOL_GUID {0x0964e5b2, 0x6459, 0x11d2, {0x8e, 0x39, 0x00, 0xa0, 0xc9, 0x69, 0x72, 0x3b}}
+
+// Structuri pentru protocolul video (GOP)
+typedef struct {
+    UINT32 Version;
+    UINT32 HorizontalResolution;
+    UINT32 VerticalResolution;
+    int    PixelFormat;
+    UINT32 PixelInformation;
+    UINT32 PixelsPerScanLine;
+} EFI_GRAPHICS_OUTPUT_MODE_INFORMATION;
+
+typedef struct {
+    UINT32 MaxMode;
+    UINT32 Mode;
+    EFI_GRAPHICS_OUTPUT_MODE_INFORMATION *Info;
+    UINTN  SizeOfInfo;
+    unsigned long long FrameBufferBase;
+    UINTN  FrameBufferSize;
+} EFI_GRAPHICS_OUTPUT_PROTOCOL_MODE;
+
+typedef struct _EFI_GRAPHICS_OUTPUT_PROTOCOL {
+    void* QueryMode;
+    void* SetMode;
+    void* Blt;
+    EFI_GRAPHICS_OUTPUT_PROTOCOL_MODE *Mode;
+} EFI_GRAPHICS_OUTPUT_PROTOCOL;
+
+// Structuri pentru manipularea fișierelor (Stick USB)
+struct _EFI_FILE_PROTOCOL;
+typedef unsigned long long (*EFI_FILE_OPEN)(struct _EFI_FILE_PROTOCOL *This, struct _EFI_FILE_PROTOCOL **NewHandle, CHAR16 *FileName, unsigned long long OpenMode, unsigned long long Attributes);
+typedef unsigned long long (*EFI_FILE_CLOSE)(struct _EFI_FILE_PROTOCOL *This);
+typedef unsigned long long (*EFI_FILE_READ)(struct _EFI_FILE_PROTOCOL *This, UINTN *BufferSize, void *Buffer);
+
+typedef struct _EFI_FILE_PROTOCOL {
+    unsigned long long Revision;
+    EFI_FILE_OPEN Open;
+    EFI_FILE_CLOSE Close;
+    void* Delete;
+    EFI_FILE_READ Read;
+    // Restul funcțiilor sunt omise pentru simplitate
+} EFI_FILE_PROTOCOL;
+
+typedef struct _EFI_SIMPLE_FILE_SYSTEM_PROTOCOL {
+    unsigned long long Revision;
+    unsigned long long (*OpenVolume)(struct _EFI_SIMPLE_FILE_SYSTEM_PROTOCOL *This, EFI_FILE_PROTOCOL **Root);
+} EFI_SIMPLE_FILE_SYSTEM_PROTOCOL;
+
+// Structuri de sistem UEFI standard
 struct _EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL;
 typedef unsigned long long (*EFI_TEXT_CLEAR_SCREEN)(struct _EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This);
 typedef unsigned long long (*EFI_TEXT_STRING)(struct _EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This, CHAR16 *String);
@@ -16,20 +81,58 @@ typedef struct _EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL {
     EFI_TEXT_CLEAR_SCREEN ClearScreen;
 } EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL;
 
+typedef unsigned long long (*EFI_LOCATE_PROTOCOL)(EFI_GUID *Protocol, void *Registration, void **Interface);
+
 typedef struct {
-    char Header;
-    CHAR16 *FirmwareVendor;
-    unsigned int FirmwareRevision;
-    void *ConsoleInHandle;
-    void *ConIn;
-    void *ConsoleOutHandle;
-    EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *ConOut; 
+    char Header[24];
+    void *AllocatePages; void *FreePages; void *GetMemoryMap; void *AllocatePool; void *FreePool;
+    void *CreateEvent; void *SetTimer; void *WaitForEvent; void *CloseEvent; void *SignalEvent;
+    void *OpenProtocol; void *CloseProtocol; void *HandleProtocol;
+    void *ProtocolsPerHandle; void *RegisterProtocolNotify; void *LocateHandleBuffer; void *LocateRegisterProtocol;
+    void *InstallMultipleProtocolInterfaces; void *UninstallMultipleProtocolInterfaces;
+    void *CalculateCrc32; void *CopyMem; void *SetMem; void *CreateEventEx;
+    EFI_LOCATE_PROTOCOL LocateProtocol; // Căutare drivere hardware
+} EFI_BOOT_SERVICES;
+
+typedef struct {
+    char Header[24];
+    CHAR16 *FirmwareVendor; UINT32 FirmwareRevision;
+    void *ConsoleInHandle; void *ConIn;
+    void *ConsoleOutHandle; EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *ConOut;
+    void *ConsoleErrorHandle; void *StdErr; void *RuntimeServices;
+    EFI_BOOT_SERVICES *BootServices;
 } EFI_SYSTEM_TABLE;
 
+// ============================================================================
+// PUNCTUL DE INTRARE ÎN SISTEMUL DE OPERARE
+// ============================================================================
 unsigned long long EfiMain(void *ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
     SystemTable->ConOut->ClearScreen(SystemTable->ConOut);
-    SystemTable->ConOut->OutputString(SystemTable->ConOut, L"=== SMR-OS v0.0.1 ===\r\n");
-    SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Booting SMR Operating System...\r\n");
+    SystemTable->ConOut->OutputString(SystemTable->ConOut, (short*)L"SMR-OS: Initializare subsisteme...\r\n");
+
+    // 1. Inițializăm placa video (GOP)
+    EFI_GUID gopGuid = EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID;
+    EFI_GRAPHICS_OUTPUT_PROTOCOL *gop = 0;
+    unsigned long long statusVideo = SystemTable->BootServices->LocateProtocol(&gopGuid, 0, (void**)&gop);
+
+    // 2. Inițializăm accesul la fișiere (Stick USB)
+    EFI_GUID fileSystemGuid = EFI_SIMPLE_FILE_SYSTEM_PROTOCOL_GUID;
+    EFI_SIMPLE_FILE_SYSTEM_PROTOCOL *fileSystem = 0;
+    unsigned long long statusFiles = SystemTable->BootServices->LocateProtocol(&fileSystemGuid, 0, (void**)&fileSystem);
+
+    if (statusVideo == 0 && gop != 0 && statusFiles == 0 && fileSystem != 0) {
+        SystemTable->ConOut->OutputString(SystemTable->ConOut, (short*)L"Placa video si Sistemul de fisiere sunt gata!\r\n");
+        
+        // Aici vom pune codul care deschide "wallpaper.bmp" și îl randează pe ecran!
+        // Momentan colorăm ecranul într-un albastru închis temporar, pregătind fundalul
+        UINT32 *fb = (UINT32*)gop->Mode->FrameBufferBase;
+        UINT32 totalPixels = gop->Mode->Info->HorizontalResolution * gop->Mode->Info->VerticalResolution;
+        for (UINT32 i = 0; i < totalPixels; i++) {
+            fb[i] = 0x00101726; // Nuanța închisă din wallpaper-ul tău
+        }
+    } else {
+        SystemTable->ConOut->OutputString(SystemTable->ConOut, (short*)L"Eroare la incarcarea driverelor hardware!\r\n");
+    }
 
     while (1) {
         __asm__("hlt");
